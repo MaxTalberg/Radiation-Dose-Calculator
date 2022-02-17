deb7368f6326a39a8502caa843d07c81e2918e79
MaxTalberg <talbergm@gmail.com>
2022-02-17 17:45:24 +0000

    Completed hollow draft

--- app.py
+++ app.py
@@ -7,10 +7,17 @@ from layout import get_dashboard_layout
 # dashboard dash app
 app = dash.Dash(name='radiation_app',
                 suppress_callback_exceptions=True,
-                external_stylesheets=[dbc.themes.BOOTSTRAP],
+                external_stylesheets=[dbc.themes.UNITED],
                 meta_tags=[{'name': 'viewport',
                             'content': 'width=device-width, initial-scale=1.0'}]
                 )
+
+theme = {
+    'dark': True,
+    'detail': '#103579',
+    'primary': '#00EA64',
+    'secondary': '#ff5716',
+}
 
 server = app.server
 app.layout = get_dashboard_layout(app=app)
